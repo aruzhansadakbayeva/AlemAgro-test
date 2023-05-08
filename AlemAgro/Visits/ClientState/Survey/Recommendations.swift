1236afd5a8e021210013b7d9a5d92fccc5cce2cc
Aruzhan Sadakbayeva <aruuunna.ss@gmail.com>
2023-05-08 19:57:03 +0600

    getHandBookMeetingRecommendations

--- AlemAgro/Visits/ClientState/Survey/Recommendations.swift
+++ AlemAgro/Visits/ClientState/Survey/Recommendations.swift
@@ -26,7 +26,7 @@ class PostmanViewModel4: ObservableObject {
     @Published var response: [PostmanResponse4] = []
     @Published var otherValue: String = ""
     func fetchData() {
-        let urlString = "http://10.200.100.17/api/manager/workspace"
+        let urlString = "http://localhost:5001/api/meetings"
         guard let url = URL(string: urlString) else {
             fatalError("Invalid URL: \(urlString)")
         }
